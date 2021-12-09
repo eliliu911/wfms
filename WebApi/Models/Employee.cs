a448e36d7ebfc217cf1224e9cdbae70668346d30
eliliu911 <eliliu911@hotmail.com>
2021-12-08 16:15:42 -0800

    Create Models

--- WebApi/Models/Employee.cs
+++ WebApi/Models/Employee.cs
@@ -1,6 +1,15 @@
-﻿namespace WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
 {
     public class Employee
     {
+        [Key] //主键
+        public int Id { get; set; }
+        [Required] //必填项
+        public string FristName { get; set; }
+        [Required] //必填项
+        public string LastName { get; set; }
+        public DateTime HiredDate { get; set; }
     }
 }
--- WebApi/Models/Task.cs
+++ WebApi/Models/Task.cs
@@ -1,6 +1,14 @@
-﻿namespace WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
 {
     public class Task
     {
+        [Key] //主键
+        public int Id { get; set; }
+        [Required] //必填项
+        public string TaskName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime Deadline { get; set; }
     }
 }
