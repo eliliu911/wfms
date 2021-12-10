9b8872e0ca22add1782683ee45d6fc2e2b31af64
eliliu911 <eliliu911@hotmail.com>
2021-12-10 12:35:31 -0800

    Create Tasks web site

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-﻿# wfms
+﻿#方案一：WebApi和Web在同一项目内，前后端不分离，使用简单的前端框架，用以减少开发时间。（本项目）
+## wfms
 Work flow manage system
 
-# 项目设计
+## 项目设计
 使用ASP.NET Core WebApi + MVC 实现接口和WebSite
 数据库使用MS SQLServer
 开发工具 Visual Studio 2022
 
-# 框架使用
+## 框架使用
 ASP.NET Core 6.0
 AspNetCore.Mvc.Razor 6.0
 EntityFrameworkCore 6.0
 Swashbuckle.AspNetCore
 
-# 前端使用
+## 前端使用
 Jquery
 Bootstrap
 Bootswtch
 
-# WebApi
+## 示例地址
+WebApi ： https://localhost:44312/
+WebSite： https://localhost:44312/web
+
+## WebApi
 EmployeesController 仅对与Employee进行CURD
 TasksController 仅对于Task进行CURD
 EmployeesGenController 对于员工信息和员工任务进行CURD
 V_EmployeeTasksController 列举全部员工和对应任务信息
+
+#方案二：前后端分离，后端使用WebApi接口，前端使用ElementUI + Vue。开发成本略高。
--- WebApi/Areas/Web/Controllers/TasksController.cs
+++ WebApi/Areas/Web/Controllers/TasksController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using WebApi.Data;
+using WebApi.Models;
 
 namespace WebApi.Areas.Web.Controllers
 {
@@ -7,13 +14,141 @@ namespace WebApi.Areas.Web.Controllers
     public class TasksController : Controller
     {
         private readonly ApplicationDbContext _context;
+
         public TasksController(ApplicationDbContext context)
         {
             _context = context;
         }
-        public IActionResult Index()
+
+        // GET: Web/Tasks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tasks.ToListAsync());
+        }
+
+        // GET: Web/Tasks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // GET: Web/Tasks/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Web/Tasks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TaskName,StartTime,Deadline")] WebApi.Models.Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        // GET: Web/Tasks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+
+        // POST: Web/Tasks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TaskName,StartTime,Deadline")] WebApi.Models.Task task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(task);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExists(task.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        // GET: Web/Tasks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.Tasks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+
+        // POST: Web/Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TaskExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
         }
     }
 }
