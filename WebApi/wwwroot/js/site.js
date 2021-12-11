374996de7557fdee60eec62ce867a06f790d416f
eliliu911 <eliliu911@hotmail.com>
2021-12-10 23:17:05 -0800

    Update  Toasts

--- WebApi/wwwroot/js/site.js
+++ WebApi/wwwroot/js/site.js
@@ -3,20 +3,3 @@
 
 // Write your JavaScript code.
 //toastr https://github.com/CodeSeven/toastr
-toastr.options = {
-    "closeButton": false,
-    "debug": false,
-    "newestOnTop": false,
-    "progressBar": false,
-    "positionClass": "toast-top-center",
-    "preventDuplicates": false,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-}
